bbb2e5bcf42129964b44c0312a47d12cb44321c2
Sathish Raja <msathis.akl@hotmail.com>
2024-10-24 17:12:12 +0800

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
-app.listen(5002, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("Server Started at http://localhost:" + PORT);
 });
